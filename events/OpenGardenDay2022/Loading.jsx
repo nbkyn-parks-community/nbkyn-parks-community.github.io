72621ec913695dd2fadd3fc7299029a03c4688d4
unknown <witkesam@gmail.com>
2022-04-30 13:48:28 -0400

    adding loading animation

---
+++ events/OpenGardenDay2022/Loading.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { fadeIn } from "react-animations";
+import styled, { keyframes } from 'styled-components';
+
+
+const Animation = keyframes`${fadeIn}`;
+const Div = styled.div`
+  animation: 1s ${Animation};
+`;
+
+
+export default function MyComponent() {
+  const [bool, setBool] = useState(false);
+  setTimeout(() => setBool(true), 5000);
+
+  return (
+    <>
+        <Div>
+          {!bool && <img src={useBaseUrl("/OpenGardenDay2022.gif")} />}
+        </Div>
+    </>
+  );
+}
